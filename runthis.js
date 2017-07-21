2e52ced3c4d8b750b51f2956dbe5e73cfa62b70c
num <numkms@gmail.com>
2017-07-22 02:43:55 +1000

    handle connect

--- runthis.js
+++ runthis.js
@@ -1,11 +1,15 @@
 var app = require('express')();
 var http = require('http').Server(app);
-
+var io = require('socket.io')(http);
 http.listen(3000, function(){
     console.log('Express has been started and listen 3000 port');
 });
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/frontend/chat.html');
+});
+//Connect handler with callback
+io.on('connection', function(socket){
+    console.log('anonymous user connected');
 });
 //Yes is hack but whatever we need just one style
 app.get('/css/main.css', function(req, res){
